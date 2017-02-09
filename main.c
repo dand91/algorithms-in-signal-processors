296ed14d6957bb171d8247fe2563c263d56d4b51
David Andersson <dand91@users.noreply.github.com>
2017-02-09 14:30:30 +0100

    Add files via upload
    main file from visualdsp

--- main.c
+++ main.c
@@ -1,100 +1,255 @@
 #include <processor_include.h>
-#include <sysreg.h>
 #include <signal.h>
 #include <string.h>
-#include <filter.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+#include <filter.h>
+
 #include "framework.h"
 
-statis int DELAY_SIZE = 3;
-static float X[DSP_BLOCK_SIZE];         // temporary input signal
-static float Y[DSP_BLOCK_SIZE];         // temporary output signal
-static float w[DSP_BLOCK_SIZE];         // temporary output signal
-static int enable;
-static int ledcounter;
+static int filter_size = 100;
+static int delay =100;
 
-void process(int sig)
+int u_s = 0;
+
+static double pm w[100];
+
+static double pm buffer[100];
+
+double y = 0;
+double e = 0;
+
+int a = 10;
+
+int index_i = 0;
+int index_j = 0;
+int index_d = 0;
+
+int i = 0;
+int j = 0;
+
+void processlms(int sig)
 {
+	
+	double mu = 0.01;
+	
+    sample_t *u30 = dsp_get_audio_u30();    /* line 2 in without mic bias, no out */
+    sample_t *u32 = dsp_get_audio_u32();    /* mic 1 and 2 in, headset out */
 
-//    double w[DSP_BLOCK_SIZE];         // Initial filter coeff.
-//
-//    for(n=0; n<DSP_BLOCK_SIZE; ++n) {
-//        w[n] = 0;
-//    }
+    buffer[u_s] = u30[0].right;
+     
+    double y = 0;
+    double e;
 
-    int n;
+    for ( i = 0; i < filter_size; ++i) {
 
-    // Get a pointer to the current audio block.
-    sample_t *audioin  = dsp_get_audio();
+        if((u_s-i) < 0){
 
-    sample_t *audioout = dsp_get_audio();
+            index_i = (u_s-i) + filter_size;
+            
+        }else{
 
-    // Copy audio from left channel to input buffer. Audio samples are 32 bit
-    // fixed-point values in the range [-1, 1] so no additional scaling is required.
-    for(n=0; n<DSP_BLOCK_SIZE; ++n) {
-        X[n] = audioin[n].left;
+            index_i = (u_s-i);
+
+        }
+
+        y += w[i]*buffer[index_i];
+
     }
 
-    if(enable) {
 
-        // Run input signal through lms
-        block_lms(X,Y,w);
+    if((u_s-delay) < 0){
+    	
+        index_d = (u_s-delay) + filter_size;
 
-    } else {
-        // Pass-through.
-        memcpy(Y, X, sizeof(Y));
+    }else{
+
+        index_d = (u_s-delay);
     }
 
-    // Copy output buffer to left and right audio channels.
-    for(n=0; n<DSP_BLOCK_SIZE; ++n) {
-        audioout[n].left = Y[n];
-        audioout[n].right = Y[n];
+    e = buffer[index_d]-y;
+    
+    for ( i = 0; i < filter_size; ++i) {
+        
+    	if((u_s-i) < 0){
+
+            index_i = (u_s-i) + filter_size;
+            
+        }else{
+        	
+            index_i = (u_s-i);
+        }
+
+        w[i]=w[i]+ mu*buffer[index_i]*e;
+  
     }
+    
+    
+    u_s = (u_s + 1) % filter_size;
+        
+    u32[0].right = e; 
+    u32[0].left  = e;
+  
+}
+
+
+void processnlms(int sig)
+{
+	double mu = 4;
+	
+    sample_t *u30 = dsp_get_audio_u30();    /* line 2 in without mic bias, no out */
+    sample_t *u32 = dsp_get_audio_u32();    /* mic 1 and 2 in, headset out */
+
+    
+    
+    buffer[u_s] = u30[0].right;
+     
+    double y = 0;
+    double e;
+
+    for ( i = 0; i < filter_size; ++i) {
+
+        if((u_s-i) < 0){
+
+            index_i = (u_s-i) + filter_size;
+            
+        }else{
+
+            index_i = (u_s-i);
+
+        }
+
+        y += w[i]*buffer[index_i];
+
+    }
+
+
+    if((u_s-delay) < 0){
+    	
+        index_d = (u_s-delay) + filter_size;
+
+    }else{
+
+        index_d = (u_s-delay);
+    }
+
+    e = buffer[index_d]-y;
+    
+    
+    double u_sum = 0;
+        
+	for ( j = 0; j < filter_size; ++j) {
+
+	        if((u_s-j) < 0){
+
+	            index_j = (u_s-j) + filter_size;
+
+	        }else{
+
+	            index_j = (u_s-j);
+
+	        }
+
+	        u_sum += buffer[index_j]*2;
+	        
+	    }
+    
+
+    for ( i = 0; i < filter_size; ++i) {
+        
+    	if((u_s-i) < 0){
+
+            index_i = (u_s-i) + filter_size;
+            
+        }else{
+        	
+            index_i = (u_s-i);
+        }
+       
+
+        w[i]=w[i] + (mu/(a+u_sum))*buffer[index_i]*e;
+        
+    }
+    
+    
+    u_s = (u_s + 1) % filter_size;
+        
+    u32[0].right = e; 
+    u32[0].left  = e;
+  
+}
+
+
+void processrls(int sig)
+{
+    sample_t *u30 = dsp_get_audio_u30();    /* line 2 in without mic bias, no out */
+    sample_t *u32 = dsp_get_audio_u32();    /* mic 1 and 2 in, headset out */;
+
+        
+    u32[0].right = u30[0].right; 
+    u32[0].left  = u30[0].right;
+  
 }
 
 static void keyboard(int sig)
 {
     unsigned int keys = dsp_get_keys();
+    unsigned int leds = 0;
 
-    if(keys & 1) {
-        enable = 1;
-    } else if(keys & 2) {
-        enable = 0;
+    if(keys & DSP_SW1) {
+        leds = 0;
+    } else if(keys & DSP_SW2) {
+        leds = DSP_D1;
+       
+    } else if(keys & DSP_SW3) {
+        leds = DSP_D2;
+    } else if(keys & DSP_SW4) {
+        leds = DSP_D1|DSP_D2;
     }
+    
+    dsp_set_leds(leds);
 }
 
 static void timer(int sig)
 {
-    ledcounter = (ledcounter+1) & 0x3F;
-    dsp_set_leds(ledcounter);
 }
 
-
 void main()
-{
-    memset(S, 0, sizeof(S));
-
-    // Setup the DSP framework
+{   
+    /*
+    Setup the DSP framework.
+    */
     dsp_init();
-
-    // Register interrupt handlers:
-    // SIG_SP1: the audio callback
-    // SIG_USR0: the keyboard callback
-    // SIG_TMZ: the timer callback
-    interrupt(SIG_SP1, process);
+    dsp_set_leds(DSP_D1 | DSP_D2);
+ 
+    /*
+    Register interrupt handlers:
+    SIG_SP1: the audio callback
+    SIG_USR0: the keyboard callback
+    SIG_TMZ: the timer callback
+    */
+    interrupt(SIG_SP1, processrls);
     interrupt(SIG_USR0, keyboard);
     interrupt(SIG_TMZ, timer);
     timer_set(9830400, 9830400);
-    timer_on();
 
-    // Enable the DSP framework.
+    /* timer_on(); */
+
+    /*
+    Start the framework.
+    */
     dsp_start();
-
-    // Everything is handled by the interrupt handlers, so just put an empty
-    // idle-loop here. If not, the program falls back to an equivalent idle-loop
-    // in the run-time library when main() returns.
+    
+    /*
+    Everything is handled by the interrupt handlers, so just put an empty
+    idle-loop here. If not, the program falls back to an equivalent idle-loop
+    in the run-time library when main() returns.
+    */
     for(;;) {
         idle();
     }
 }
+
+
+
+
